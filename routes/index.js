9de5d5f2ce96033045504e30d28e30f3686cae0f
jeongwooklee <wook.fla@gmail.com>
2013-04-23 18:02:58 +0900

    add sample markdown file - javascript/FB.api

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    type: 'main'
+  });
 };
 exports.reference = function(req, res){
   var type = req.params.type,
       name = req.params.name.replace(/\./g, '');
-  res.render('reference/' + type + '/' + name);
+  res.render('reference/' + type + '/' + name, {
+    type: type
+  });
 };
